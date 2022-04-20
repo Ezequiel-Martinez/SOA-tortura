19abdf5cc9d12e81d70558736c9d55804c5db308
mauro_unlam <maorlandi@alumno.unlam.edu.ar>
2022-04-19 22:50:12 -0300

    agrego el timer que faltaba en keypad_game, algunos comentarios, y otros cambios menores

--- codigo.cpp
+++ codigo.cpp
@@ -4,30 +4,34 @@
 #include <LiquidCrystal.h>	//para el lcd
 
 //Defines
-//#define DEBUG_MODE				//para hacer debug por consola
-#define NUM_ROWS_LCD 3			//cantidad de filas que usamos en el keypad
-#define NUM_COLS_LCD 3			//cantidad de columnas que usamos en el keypad
-#define NUM_ROWS_KEYPAD 3		//cantidad de filas que usamos en el keypad
-#define NUM_COLS_KEYPAD 3		//cantidad de columnas que usamos en el keypad
-#define RANDOM_NUM_LENGTH 11	//cantidad de digitos que queremos que tenga el numero random ( contemplando el '\0')
-#define BUZZER_COOLDOWN 1000	//cantidad de tiempo minimo que debe pasar entre un button_game y otro ( es un cooldown ) medido en milisegundos
-#define MAX_BUTTON_PRESS_DELAY 10 //cantidad de tiempo que maximo que tiene el usuario para presionar el boton luego de escuchar el sonido
+//#define DEBUG_MODE					//para hacer debug por consola
+#define NUM_ROWS_LCD 3					//cantidad de filas que usamos en el keypad
+#define NUM_COLS_LCD 3					//cantidad de columnas que usamos en el keypad
+#define NUM_ROWS_KEYPAD 3				//cantidad de filas que usamos en el keypad
+#define NUM_COLS_KEYPAD 3				//cantidad de columnas que usamos en el keypad
+#define RANDOM_NUM_LENGTH 11			//cantidad de digitos que queremos que tenga el numero random ( contemplando el '\0')
+#define BUZZER_COOLDOWN 1000			//cantidad de tiempo minimo que debe pasar entre un button_game y otro ( es un cooldown ) medido en milisegundos
+#define MAX_BUTTON_PRESS_DELAY 10000 	//cantidad de tiempo maximo que tiene el usuario para presionar el boton luego de escuchar el sonido
+#define KEYPAD_GAME_TIME_REDUCTION 500 	//cantidad de tiempo que se va a restar a max_keypad_game_time cada vez que el usuario gana
 
 typedef struct{
 
 	long start_time;
 	long finish_time;
 
-} Timer;
+} t_timer;
 
 //Variables globales
-int button_pin;
-Timer buzzer_cooldown;
-char random_number_buffer[RANDOM_NUM_LENGTH];
+t_timer buzzer_cooldown;
+t_timer lcd_clear_timer;	//timer que uso para saber cuanto lleva el numero random en el lcd
+
 LiquidCrystal lcd(1,2,3,4,5,6);	//los parametros son los pines a los cuales esta conectado el lcd
 
+int button_pin;
 int print_on_lcd;	//variable que indica si es necesario hacer un print en el lcd o no
 int n_key_pressed;	//cantidad de keys que se presionaron en el keypad
+int max_keypad_game_time;	//cantidad maxima de tiempo que va a tener el numero random para estar en el lcd en milisegundos ( lo pongo como variable para poder modificarlo )
+char random_number_buffer[RANDOM_NUM_LENGTH];
 
 //mapa de teclas que vamos a usar del keypad
 char key_map[NUM_ROWS_KEYPAD][NUM_COLS_KEYPAD] = {
@@ -45,7 +49,7 @@ Keypad keypad = Keypad(makeKeymap(key_map),row_pins,col_pins,NUM_ROWS_KEYPAD,NUM
 void button_game(void);
 void keypad_game(void);
 void generate_random_number(char *buffer);
-void print_string_on_lcd(const char *buffer);
+void print_string_on_lcd(const char *buffer, int row, int column);
 
 void setup()
 {
@@ -66,6 +70,7 @@ void setup()
   //Keypad
   print_on_lcd = 1;
   n_key_pressed = 0;
+  max_keypad_game_time = 1500;
   pinMode(A2,INPUT);
   pinMode(A3,INPUT);
   pinMode(A4,INPUT);
@@ -80,7 +85,8 @@ void loop()
   
   if(print_on_lcd){
 	generate_random_number(random_number_buffer);    
-  	print_string_on_lcd(random_number_buffer);
+  	print_string_on_lcd(random_number_buffer,0,3);
+  	lcd_clear_timer.start_time = millis();
   }
   
   button_game(); 
@@ -88,6 +94,7 @@ void loop()
   
 }
 
+//Funcion que se encarga con todo lo relacionado con el boton
 void button_game(void){
 
   int button_state;
@@ -110,6 +117,8 @@ void button_game(void){
 #endif
     
     //aca hay que poner un delay, porque sino no hay manera de darle tiempo al usuario a presionar el boton
+    //test
+    delayMicroseconds(MAX_BUTTON_PRESS_DELAY);
     button_state = analogRead(button_pin);
 
     //El 1023 es por el valor maximo que retorna analogRead()
@@ -132,6 +141,7 @@ void button_game(void){
   
 }
 
+//Funcion que se encarga de todo lo relacionado con el keypad
 void keypad_game(const char *random_number_buffer){
   
   char key;
@@ -141,15 +151,35 @@ void keypad_game(const char *random_number_buffer){
   if(key == NO_KEY){
   	return;
   }
+  
+#ifdef DEBUG_MODE
+    Serial.print("Tecla presionada");
+    Serial.print(key);
+#endif    
+  
+  lcd_clear_timer.finish_time = millis();
+  
+  if(lcd_clear_timer.finish_time - lcd_clear_timer.start_time > max_keypad_game_time){
+
+#ifdef DEBUG_MODE
+    Serial.println("Se alcanzo el tiempo maximo para mostrar el numero en el lcd");
+#endif    
+
+  	lcd.clear();
+  	lcd_clear_timer.start_time = lcd_clear_timer.finish_time;
+
+  }  
     
   if(key != *(random_number_buffer + n_key_pressed)){
     
 #ifdef DEBUG_MODE
-    Serial.print("El numero ingresado es erroneo");
+    Serial.print("El numero ingresado es erroneo. Se esperaba: ");
+    Serial.print(*(random_number_buffer + n_key_pressed));
+    Serial.print(" , y se recibio: ");
+    Serial.print(key);
 #endif    
     
-    //si pierdo necesito indicar que se tiene que mostrar otro numero en el lcd
-    print_on_lcd = 1;
+    print_on_lcd = 1; //si pierdo necesito indicar que se tiene que mostrar otro numero en el lcd
     n_key_pressed = 0;
     //encender el led indicando que se electrocuto    
     return;
@@ -164,8 +194,8 @@ void keypad_game(const char *random_number_buffer){
     Serial.println("El numero ingresado es correcto");
 #endif        
     
-    //si gano necesito indicar que se tiene que mostrar otro numero en el lcd
-    print_on_lcd = 1;
+    print_on_lcd = 1; //si gano necesito indicar que se tiene que mostrar otro numero en el lcd
+    max_keypad_game_time -= KEYPAD_GAME_TIME_REDUCTION; //el siguiente numero va a tener menos tiempo en el lcd
     //ver que hacer cuando ingresa el numero correctamente
     
   }
@@ -176,22 +206,29 @@ void keypad_game(const char *random_number_buffer){
 void generate_random_number(char *buffer){
 
   for(int i = 0; i < RANDOM_NUM_LENGTH; i++){
-  	*(buffer + i) = random(49,58);
+  	*(buffer + i) = random(49,58);	//genero un numero entre 1-9
   }
   
   *(buffer + RANDOM_NUM_LENGTH - 1) = '\0';
 
 #ifdef DEBUG_MODE
+  Serial.print("Numero random generado:");
   Serial.println(buffer);
 #endif
   
 }
 
-void print_string_on_lcd(const char *buffer){
+//Funcion que se encarga de mostrar en el lcd el contenido del buffer que recibe por parametro, row y column son para posicionar el cursor del lcd
+void print_string_on_lcd(const char *buffer, int row, int column){
   
-  lcd.clear();
-  lcd.setCursor(0,3);
+  lcd.clear(); //limpio cualquier cosa que tenga el lcd
+  lcd.setCursor(row,column); //para que el numero quede centrado
   lcd.print(buffer);
   print_on_lcd = 0;
+  
+#ifdef DEBUG_MODE
+  Serial.print("Buffer a imprimir en el lcd:");
+  Serial.println(buffer);
+#endif  
   
 }
